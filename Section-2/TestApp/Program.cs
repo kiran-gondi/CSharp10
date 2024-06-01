0758323b30d76d46bc4edbb83e7a5d1e0f75bc9b
kiran.gondi <kiran.gondi@planview.com>
2024-06-01 22:57:04 +0530

    Console.WriteLine example's

---
+++ Section-2/TestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿class Sample
+{
+  static void Main()
+  {
+    System.Console.Write("Welcome");
+    System.Console.Write(" to");
+    System.Console.Write(" C#");
+
+    System.Console.ReadKey();
+
+    System.Console.Clear();
+    
+    System.Console.Write(" C# Thanks");
+    System.Console.ReadKey(true);
+
+  }
+}
+
+
+/*class Sample
+{
+  static void Main()
+  {
+    System.Console.WriteLine("Hello World");
+    System.Console.ReadKey();
+  }
+}*/
