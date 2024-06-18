7973b0a413b3d0f7c880174850a507a20b8952d1
kiran.gondi <kiran.gondi@planview.com>
2024-06-18 15:03:10 +0530

    Recusion Example

---
+++ Section-6/RecursionExample/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace RecursionExample
+{
+
+  class Sample
+  {
+    public double Factorial(int number)
+    {
+      if (number == 0)
+      {
+        return 1;
+      }
+      else
+      {
+        return number * Factorial(number-1);
+      }
+    }
+  }
+
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+
+      Console.WriteLine($"Enter a number: ");
+      int n = int.Parse(Console.ReadLine());
+
+      Sample sample = new Sample();
+
+      double result = sample.Factorial(n);
+      Console.WriteLine($"Factorial of {n} is {result}");
+
+      Console.ReadKey();  
+    }
+  }
+}
