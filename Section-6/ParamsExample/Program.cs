4dea56fcb7e68680d1cfb3d918c0ef4b82fac9e3
kiran.gondi <kiran.gondi@planview.com>
2024-06-18 14:35:20 +0530

    Parameter Modifier - Params

---
+++ Section-6/ParamsExample/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ParamsExample
+{
+  class Student
+  {
+    public void DisplaySubjects(params string[] subjects)
+    {
+      //Console.WriteLine(subjects[0]);
+      //Console.WriteLine(subjects[1]);
+      //Console.WriteLine(subjects[2]);
+      //Console.WriteLine(subjects[3]);
+      foreach (var subject in subjects)
+      {
+        Console.WriteLine(subject);
+      }
+    }
+  }
+
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Student student = new Student();  
+
+      student.DisplaySubjects("EC", "CS", "M1", "M4", "D1", "EC11");
+
+      Console.ReadKey();
+    }
+  }
+}
