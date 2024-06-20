6c4c2b1cea55d5e65edb12d5db71ec9f1b63c3eb
kiran.gondi <kiran.gondi@planview.com>
2024-06-20 12:04:08 +0530

    Indexer Overloading

--- Section-9/IndexersExample/ClassLibrary1/Class1.cs
+++ Section-9/IndexersExample/ClassLibrary1/Class1.cs
@@ -2,6 +2,7 @@
 {
   //private
   private string[] _brands = new string[] {"Swift", "Hyundai", "Benz" };
+  private string[] _names = new string[] { "first", "second", "third" };
 
   //public indexer
   public string this[int index]
@@ -13,4 +14,13 @@
     get { return this._brands[index]; }
   }
 
+  //indexer overloading
+  public string this[string name]
+  {
+    set
+    {
+      this._brands[Array.IndexOf(_names, name)] = value;
+    }
+    get { return this._brands[Array.IndexOf(_names, name)]; }
+  }
 }
