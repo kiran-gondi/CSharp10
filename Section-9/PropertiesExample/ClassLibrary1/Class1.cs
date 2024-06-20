502294ef21e9046298416dc9f2f71bf8482cdb13
kiran.gondi <kiran.gondi@planview.com>
2024-06-20 10:48:22 +0530

    Creating Properties

---
+++ Section-9/PropertiesExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,62 @@
+﻿public class Employee
+{
+  private int _empId;
+  private string _firstName;
+  private string _job;
+
+  public int EmployeeId
+  {
+    set {
+      if(value >= 100)
+      _empId = value; 
+    }
+    get { return _empId; }
+  }
+
+  public string FirstName
+  {
+    set { _firstName = value; }
+    get { return _firstName; }
+  }
+
+  public string Job
+  {
+    set { _job = value; }
+    get { return _job; }
+  }
+
+  private static string? _companyName;
+
+  public static string CompanyName
+  {
+    set { 
+      if(value.Length <= 20) 
+      { 
+        _companyName = value;
+      }
+    }
+    get { return _companyName ?? string.Empty; }
+  }
+
+    public Employee()
+    {
+        this._empId = 1;
+    }
+
+    public Employee(int empId, string firstName)
+    {
+        this._empId = empId;
+        this._firstName = firstName;
+    }
+    public Employee(int empId, string firstName, string job)
+    {
+      this._empId = empId;
+      this._firstName = firstName;
+      this._job = job;
+    }
+
+    static Employee()
+    {
+      CompanyName = "Uncle Bob's Org";
+    }
+}
