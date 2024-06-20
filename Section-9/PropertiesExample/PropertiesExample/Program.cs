e8426327b684d5c9e0b0637af378714796fd519e
kiran.gondi <kiran.gondi@planview.com>
2024-06-20 11:27:01 +0530

    Read-Only & Write-Only Property

--- Section-9/PropertiesExample/PropertiesExample/Program.cs
+++ Section-9/PropertiesExample/PropertiesExample/Program.cs
@@ -10,6 +10,8 @@
       emp1.EmployeeId = 10;
       emp1.FirstName = "Scott";
       emp1.Job = "Manager";
+      //emp1.Salary = 20000; NOT ALLOWED
+      emp1.Tax = 50;
       Employee emp2 = new Employee(102, "Allen"); //constructor 2
       emp2.Job = "Asst. Manager";
       Employee emp3 = new Employee(103, "Anna", "Clerk"); //constructor 1
@@ -21,18 +23,22 @@
       System.Console.WriteLine(emp1.EmployeeId);
       System.Console.WriteLine(emp1.FirstName);
       System.Console.WriteLine(emp1.Job);
+      System.Console.WriteLine(emp1.Salary);
+      System.Console.WriteLine("Calculated Net Salary: " + emp1.CalculateNetSalary());
       System.Console.WriteLine();
 
       System.Console.WriteLine("Second employee:");
       System.Console.WriteLine(emp2.EmployeeId);
       System.Console.WriteLine(emp2.FirstName);
       System.Console.WriteLine(emp2.Job);
+      System.Console.WriteLine(emp2.Salary);
       System.Console.WriteLine();
 
       System.Console.WriteLine("Third employee:");
       System.Console.WriteLine(emp3.EmployeeId);
       System.Console.WriteLine(emp3.FirstName);
       System.Console.WriteLine(emp3.Job);
+      System.Console.WriteLine(emp3.Salary);
       System.Console.WriteLine();
 
       System.Console.ReadKey();
