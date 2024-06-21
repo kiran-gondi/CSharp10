cf9030342ed76cd7cf4d2bb61e03af9a0162dd45
kiran.gondi <kiran.gondi@planview.com>
2024-06-21 18:23:16 +0530

    Readonly structures

---
+++ Section-14/ReadOnlyStructureExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿public readonly struct Marvel
+{
+    //field
+    private readonly string _characterName;
+
+    //public readonly property
+    public string CharacterName { get {
+      return _characterName;
+    } 
+  }
+
+  //public method
+  public void PrintCharacteName()
+  {
+    Console.WriteLine(this.CharacterName);
+  }
+
+    //constructors
+    public Marvel(string characterName)
+    {
+        this._characterName = characterName;
+    }
+}
---
+++ Section-14/ReadOnlyStructureExample/ReadOnlyStructureExample/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+  static void Main()
+  {
+    //create structure instance
+    Marvel marvel = new Marvel("Thanos");
+      
+
+    //invoke the propertis and methods
+    Console.WriteLine(marvel.CharacterName);
+    //marvel.CharacterName = "Thor";
+    marvel.PrintCharacteName();
+
+    Console.ReadKey();
+  }
+}
