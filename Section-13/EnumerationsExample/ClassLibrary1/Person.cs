ba1d21da6ec6840eed59b4a2829ca1497603ab33
kiran.gondi <kiran.gondi@planview.com>
2024-06-21 17:20:18 +0530

    Enumerations or Enum

---
+++ Section-13/EnumerationsExample/ClassLibrary1/Person.cs
@@ -0,0 +1,15 @@
+﻿public class Person
+{
+  public string Name { get; set; }
+  public string Email { get; set; }
+
+  public AgeGroupEnumeration AgeGroup { get; set; }
+}
+
+public enum AgeGroupEnumeration : short //Can't use string here
+{
+  Child,
+  Teenager,
+  Adult,
+  Senior
+}
---
+++ Section-13/EnumerationsExample/EnumerationsExample/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+  public static void Main()
+  {
+    
+    Person p = new Person();
+    p.Name = "jon";
+    p.Email = "ramu@google.com";
+    p.AgeGroup = AgeGroupEnumeration.Senior;
+
+    Console.WriteLine(p.AgeGroup);
+    Console.ReadKey();
+  }
+}
