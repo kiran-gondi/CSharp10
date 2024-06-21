d63e499e6a153b231df413f52049ec6af46144ab
kiran.gondi <kiran.gondi@planview.com>
2024-06-21 17:45:19 +0530

    Creating Structures

---
+++ Section-14/StructuresExample/ClassLibrary1/Category.cs
@@ -0,0 +1,34 @@
+﻿public struct Category
+{
+  //private field
+  private int _categoryId;
+  private string _categoryName;
+
+  //public fields
+  public int CategoryId
+  {
+    get { return _categoryId; }
+    set 
+    {
+      if(value >= 1 && value <= 100) { 
+      _categoryId = value;
+      }
+    }
+  }
+
+  public string Categoryname
+  {
+    get { return _categoryName; }
+    set 
+    {
+      if(value.Length <= 40)
+        _categoryName = value; 
+    }
+  }
+
+  public int GetCategoryNameLength()
+  {
+    return this._categoryName.Length;
+  }
+
+}
---
+++ Section-14/StructuresExample/StructuresExample/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+  public static void Main()
+  {
+    //Create structure instance
+    Category category = new Category();
+
+    //initialize fields through properties.
+    category.CategoryId = 20;
+    category.Categoryname = "General";
+
+    //access methods
+    Console.WriteLine(category.CategoryId);
+    Console.WriteLine(category.Categoryname);
+    Console.WriteLine(category.GetCategoryNameLength());
+
+    Console.ReadKey();  
+  }
+}
