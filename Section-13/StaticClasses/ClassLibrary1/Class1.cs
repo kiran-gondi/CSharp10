c12071fd3a9d06a424f71da8b0b7ef8e3d4a38c0
kiran.gondi <kiran.gondi@planview.com>
2024-06-21 17:12:27 +0530

    Static Classes

---
+++ Section-13/StaticClasses/ClassLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿public static class Country
+{
+  public static string CountryName = "INDIA";
+  public static int NoOfStates = 28;
+
+  public static int GetTotalNoOfUnionTerritories()
+  {
+    return 8;
+  }
+}
---
+++ Section-13/StaticClasses/StaticClasses/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+  public static void Main()
+  {
+
+    //Create object of static class
+    //Country c = new Country();
+    Console.WriteLine(Country.CountryName);
+    Console.WriteLine(Country.NoOfStates);
+    Console.WriteLine(Country.GetTotalNoOfUnionTerritories());
+
+    Console.ReadKey();
+  }
+}
