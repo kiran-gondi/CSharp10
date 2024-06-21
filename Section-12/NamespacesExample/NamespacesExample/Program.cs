8fe2e061c5a5e67aae6c7f1fc874944f4172b702
kiran.gondi <kiran.gondi@planview.com>
2024-06-21 16:24:09 +0530

    Creating Namespaces

---
+++ Section-12/NamespacesExample/NamespacesExample/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NamespacesExample
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      HR.IManager mgr = new HR.Manager();
+
+      FrontOffice.FrontOfficeExecutive frontOfficeExecutive = new FrontOffice.FrontOfficeExecutive();
+      FrontOffice.CustomerEnquiry customerEnquiry = new FrontOffice.CustomerEnquiry();
+
+      Console.ReadKey();
+    }
+  }
+}
