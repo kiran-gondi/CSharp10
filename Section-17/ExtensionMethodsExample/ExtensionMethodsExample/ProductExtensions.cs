5c98fcf463b339d9f5b5219f08fef8598fd34dbd
kiran.gondi <kiran.gondi@planview.com>
2024-06-23 21:39:38 +0530

    Extension Methods

---
+++ Section-17/ExtensionMethodsExample/ExtensionMethodsExample/ProductExtensions.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary1;
+
+namespace ExtensionMethodsExample
+{
+  //static class for extension method
+  public static class ProductExtensions
+  {
+    //extension method for product class
+    public static double GetDiscount(this Product product)
+    {
+      return product.ProductCost * product.DiscountPercentage / 100;
+    }
+  }
+}
