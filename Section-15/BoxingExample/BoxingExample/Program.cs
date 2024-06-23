618f5d1d26e1099630b7aa05a27f1eb257189a38
kiran.gondi <kiran.gondi@planview.com>
2024-06-23 18:01:47 +0530

    Boxing - VRT

---
+++ Section-15/BoxingExample/BoxingExample/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BoxingExample
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //primitive variable
+      int x = 10;
+
+      //Boxing (value-type to reference-type)
+      object obj = x;
+
+      Console.WriteLine(x);
+      Console.WriteLine(obj);
+
+
+      Console.ReadKey();
+    }
+  }
+}
