9ec7b90844c256e3283abaec711f35ab3b560ddb
kiran.gondi <kiran.gondi@planview.com>
2024-06-23 21:51:19 +0530

    Pattern Matching

---
+++ Section-17/PatternMatchingExmaple/PatternMatchingExmaple/Program.cs
@@ -0,0 +1,34 @@
+﻿using ClassLibrary1;
+
+namespace PatternMatchingExmaple
+{
+  class Program
+  {
+    static void Main()
+    {
+      //reference variable of parent class
+      ParentClass pc;
+
+      //object of child class
+      pc = new ChildClass() { x = 10, y = 20 };
+
+      //access parent class's members
+      //Console.WriteLine(pc.x);
+
+      //Old way
+      //if (pc is ChildClass) { 
+      //  ChildClass cc = (ChildClass)pc;
+      //  Console.WriteLine(cc.x);
+      //  Console.WriteLine(cc.y);
+      //}
+
+      //C# PatternMatching
+      if(pc is ChildClass cc)
+      {
+        Console.WriteLine(cc.x + " & " + cc.y);
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
