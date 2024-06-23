b630c737cbaba0fc523e979a44e5addc81275874
kiran.gondi <kiran.gondi@planview.com>
2024-06-23 17:11:04 +0530

    Overview of System.Object class

---
+++ Section-15/ObjectClassExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,9 @@
+﻿public class Person
+{
+
+}
+
+public class Supplier : Person
+{
+
+}
---
+++ Section-15/ObjectClassExample/ObjectClassExample/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+  public static void Main()
+  {
+    try
+    {
+      Person p;
+      System.Object abc = new Person();
+      p = new Supplier();
+
+      int a = 10;
+      System.Object inta = a;
+    }
+    catch (Exception)
+    {
+
+      throw;
+    }
+    
+
+    Console.ReadKey();
+  }
+}
