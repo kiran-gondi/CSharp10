9ba61240918157aecd36a0ceefc21c5f9c98cea1
kiran.gondi <kiran.gondi@planview.com>
2024-06-23 22:01:05 +0530

    Implicitly Typed Variables

---
+++ Section-17/ImplicitlyTypedVariablesExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace namespace1
+{
+  namespace namespace2
+  {
+    namespace namespace3
+    {
+      public class Person
+      {
+          public string PersonName { get; set; }
+      }
+    }
+  }
+  
+}
