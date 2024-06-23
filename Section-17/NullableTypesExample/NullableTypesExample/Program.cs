653b7406e57b534366193dedf1295782d67ce718
kiran.gondi <kiran.gondi@planview.com>
2024-06-23 19:41:08 +0530

    Nullable Types

---
+++ Section-17/NullableTypesExample/NullableTypesExample/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NullableTypesExample
+{
+
+  class Person
+  {
+      //public Nullable<int> NoOfChildren { get; set; }
+          //OR
+      public int? NoOfChildren { get; set; }
+  }
+
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      //create object
+      Person p1 = new Person() { NoOfChildren = 2 };
+      Person p2 = new Person() { NoOfChildren = null };
+      Person p3 = null; //Valid for classes
+
+      Console.WriteLine(p1.NoOfChildren);
+      //if (p2.NoOfChildren != null)
+      if (p2.NoOfChildren.HasValue)
+      {
+        int x = p2.NoOfChildren.Value;
+        Console.WriteLine(x);
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
