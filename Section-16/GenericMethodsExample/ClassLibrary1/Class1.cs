5fff577b5cf3a7ca42e3558c0a1fd7188493bf4d
kiran.gondi <kiran.gondi@planview.com>
2024-06-23 19:19:49 +0530

    Generic Methods

---
+++ Section-16/GenericMethodsExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿public class Employee {
+    public int Salary { get; set; }
+}
+
+public class Student
+{
+    public int Marks { get; set; }
+}
+
+//a class with generic method
+public class Sample
+{
+  public void PrintData<T>(T obj) where T: class
+  {
+    if (obj.GetType() == typeof(Student)) { 
+      
+      Student tempStudent = obj as Student;
+      Console.WriteLine(obj.GetType() + " marks is " + tempStudent.Marks);
+    } 
+    else if(obj.GetType() == typeof(Employee)) { 
+      Employee tempEmployee = obj as Employee;
+      Console.WriteLine(obj.GetType() + " salary is " + tempEmployee.Salary);
+    }
+  }
+}
---
+++ Section-16/GenericMethodsExample/GenericMethodsExample/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+  public static void Main()
+  {
+    //Create instance of Sample class
+    Employee employee = new Employee() { Salary = 10000 };
+    Student student = new Student() { Marks = 55 };
+    Sample s = new Sample() ;
+    
+    //call the generic method
+    s.PrintData<Employee>(employee);
+    s.PrintData<Student>(student);
+
+    Console.ReadKey();
+
+  }
+}
