4b86c01f78c44aed053da500c5a654f846501284
kiran.gondi <kiran.gondi@planview.com>
2024-06-23 22:46:52 +0530

    Dynamically Typed Variables

---
+++ Section-17/DynamicallyTypedVariablesExample/DynamicallyTypedVariablesExample/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DynamicallyTypedVariablesExample
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //dynamically typed variable
+      dynamic x;
+
+      x = 10;
+
+      x = "Hello";
+
+      x = new Student() { Id = 123, Name = "Ramu" };
+
+      //Console.WriteLine(x.StudentName); Runtime error or exception
+      Console.WriteLine(x.Name);
+      Console.WriteLine(x.Id);
+
+      Console.ReadKey();
+    }
+  }
+
+  internal class Student
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Student()
+    {
+    }
+  }
+}
