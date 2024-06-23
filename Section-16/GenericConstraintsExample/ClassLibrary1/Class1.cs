cfe096c293b664f180bfe1fe65d266b79ff0a648
kiran.gondi <kiran.gondi@planview.com>
2024-06-23 19:06:22 +0530

    Generic Constraints

---
+++ Section-16/GenericConstraintsExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿public abstract class Student
+{
+    public abstract int Marks { get; set; }
+}
+
+public class GraduateStudent : Student
+{
+  public override int Marks { get; set; }
+}
+
+public class PostGraduateStudent : Student
+{
+  public override int Marks { get; set; }
+}
+
+//generic class with constraints (want to accept Student or its child classes only).
+public class MarksPrinter<T> where T : Student
+{
+  public T student;
+  public void PrintMarks()
+  {
+    Student tempStudent = (Student)student;
+    Console.WriteLine(student.GetType() + " marks is " + tempStudent.Marks); 
+  }
+}
