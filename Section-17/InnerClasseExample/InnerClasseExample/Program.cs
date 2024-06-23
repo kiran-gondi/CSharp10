5b5314d4efb85bc62f4202b8869a3ab5828ccf61
kiran.gondi <kiran.gondi@planview.com>
2024-06-23 23:05:16 +0530

    Inner Classes

---
+++ Section-17/InnerClasseExample/InnerClasseExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary1;
+
+namespace InnerClasseExample
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //create an object of inner class
+      ClassLibrary1.MarksCalculation.CalclulationHelper ch = new MarksCalculation.CalclulationHelper();
+
+      //call the inner class's method
+      //Console.WriteLine(ch.Multiply(20, 3));
+
+      //call outer class's method
+      ClassLibrary1.MarksCalculation mc = new MarksCalculation();
+      Student s = new Student() {SecuredMarks = 70, MaxMarks = 100 };
+      mc.CalculatePercentage(s);
+      Console.WriteLine(s.Percentage.ToString());
+
+      Console.ReadKey();
+    }
+  }
+}
