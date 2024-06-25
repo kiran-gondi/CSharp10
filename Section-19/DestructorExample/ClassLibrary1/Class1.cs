2be4761b49961f2322a0e5982d2c34d5bb1dde72
kiran.gondi <kiran.gondi@planview.com>
2024-06-25 19:31:47 +0530

    Destructors

---
+++ Section-19/DestructorExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace ClassLibrary1
+{
+  //public class Sample
+  //{
+  //  //constructor
+  //  public Sample()
+  //  {
+  //    //file & db connection logic here
+  //    Console.WriteLine("File is opened");
+  //  }
+
+  //  //destructor
+  //  ~Sample() 
+  //  {
+  //    //file & db connection logic here
+  //    Console.WriteLine("File is closed here");
+  //  }
+
+  //}
+
+  public class Sample
+  {
+    //constructor
+    public Sample()
+    {
+      //file & db connection logic here
+      Console.WriteLine("File is opened");
+    }
+
+    //destructor
+    ~Sample()
+    {
+      //file & db closing logic here
+      Console.WriteLine("File is closed");
+    }
+  }
+}
---
+++ Section-19/DestructorExample/DestructorExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using ClassLibrary1;
+
+class Program
+{
+
+  //NOTE: Use the .NET Framework 4.8 to get the output for some reason we are unable to see the output for .NETCore 8.0 version.
+  public static void Main()
+  {
+    //create object of sample class
+    Sample s = new Sample();
+
+  }//destructor gets called here automatically
+}
