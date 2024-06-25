45f191df0459a25639818e4c1c7edfb551a9cbd1
kiran.gondi <kiran.gondi@planview.com>
2024-06-25 23:46:55 +0530

    Events

---
+++ Section-20/EventsExample/ClassLibrary1/Publisher.cs
@@ -0,0 +1,37 @@
+﻿namespace ClassLibrary1
+{
+  //delegate type
+  public delegate void MyDelegateType(int a,  int b);
+
+  //publisher
+  public class Publisher
+  {
+    //private delegate
+    private MyDelegateType myDelegate;
+
+    //step1: create event
+    public event MyDelegateType myEvent
+    {
+      add
+      {
+        myDelegate += value;
+      }
+
+      remove 
+      { 
+        myDelegate -= value;
+      }
+    }
+
+    public void RaiseEvent(int a, int b)
+    {
+      //step2: raise event
+      if (this.myDelegate != null)
+      {
+        this.myDelegate(a, b);
+      }
+
+    }
+
+  }
+}
---
+++ Section-20/EventsExample/EventsExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace EventsExample
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      //create object of Subscriber class
+      Subscriber subscriber = new Subscriber();
+
+      //create object of Publisher class
+      Publisher publisher = new Publisher();
+
+      //handle the event(or)subscribe to event
+      publisher.myEvent += subscriber.Add;
+
+      //invoke the event
+      publisher.RaiseEvent(10, 20);
+
+      Console.ReadKey();
+    }
+  }
+}
