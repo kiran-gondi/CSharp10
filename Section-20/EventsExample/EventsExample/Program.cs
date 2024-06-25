8bf343336e073b715bdd223901c6f0acd8945b98
kiran.gondi <kiran.gondi@planview.com>
2024-06-26 00:08:51 +0530

    Inline Lambda Expressions

--- Section-20/EventsExample/EventsExample/Program.cs
+++ Section-20/EventsExample/EventsExample/Program.cs
@@ -23,11 +23,14 @@ namespace EventsExample
       //};
 
       //Lambda Expression
-      publisher.myEvent += (a, b) =>
-      {
-        int c = a + b;
-        Console.WriteLine(c);
-      };
+      //publisher.myEvent += (a, b) =>
+      //{
+      //  int c = a + b;
+      //  Console.WriteLine(c);
+      //};
+
+      //Inline Lambda Expression
+      publisher.myEvent += (a, b) => Console.WriteLine(a+b);
 
       //invoke the event
       publisher.RaiseEvent(10, 20);
