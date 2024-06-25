b79e14ca41c058d4def276f113d0fc3243e1b419
kiran.gondi <kiran.gondi@planview.com>
2024-06-25 19:49:39 +0530

    IDisposable

---
+++ Section-19/IDisposableExample/IDisposableExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace IDisposableExample
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //create object using "using structure"
+      using (Sample s = new Sample())
+      {
+        s.DisplayDataFromDatabase();
+      }
+
+      Console.WriteLine("Some other work here");
+      Console.ReadKey();
+    }
+  }
+}
