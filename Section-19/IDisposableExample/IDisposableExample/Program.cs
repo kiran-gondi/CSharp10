0f844295d86e7c73474b87c44009915915198b97
kiran.gondi <kiran.gondi@planview.com>
2024-06-25 19:56:45 +0530

    Using Declaration

--- Section-19/IDisposableExample/IDisposableExample/Program.cs
+++ Section-19/IDisposableExample/IDisposableExample/Program.cs
@@ -5,15 +5,25 @@ namespace IDisposableExample
 {
   internal class Program
   {
+    static void Work()
+    {
+      //create object using "using declaration"
+      using Sample s = new Sample();
+        s.DisplayDataFromDatabase();
+    }//Dispose gets called here
+
     static void Main(string[] args)
     {
       //create object using "using structure"
-      using (Sample s = new Sample())
-      {
-        s.DisplayDataFromDatabase();
-      }
+      //using (Sample s = new Sample())
+      //{
+      //  s.DisplayDataFromDatabase();
+      //}
+
+      Work();
 
       Console.WriteLine("Some other work here");
+
       Console.ReadKey();
     }
   }
