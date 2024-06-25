d57365dbf19aeb2412e3dabb048dd81a7b4f1bad
kiran.gondi <kiran.gondi@planview.com>
2024-06-25 21:28:43 +0530

    Multi Cast Delegates

---
+++ Section-20/MulitCastDelegatesExample/MulitCastDelegatesExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulitCastDelegatesExample
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //create object of sample
+      Sample s = new Sample();
+
+      //Create reference variable of MyDelegateType
+      MyDelegatType myDelegate;
+
+      //add ref of first target method
+      //myDelegate = new MyDelegatType(s.Add);
+      myDelegate = s.Add;
+
+      //add ref of second target method
+      myDelegate += s.Multiply;
+
+      //invoke both target methods; first Add method; and then Multiply method
+      myDelegate.Invoke(4, 5);
+
+      Console.ReadKey();
+
+    }
+  }
+}
