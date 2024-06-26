3f20ade1bf3db8f461bf3fbe4e6ca71d0d8edf57
kiran.gondi <kiran.gondi@planview.com>
2024-06-26 21:17:32 +0530

    System.Array.Sort

---
+++ Section-21/ArraySortExample/ArraySortExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ArraySortExample
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] a = new int[5] { 50, 40, 30, 20, 10 };
+
+      Array.Sort(a);
+
+      foreach (int i in a)
+      {
+        Console.WriteLine(i);
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
