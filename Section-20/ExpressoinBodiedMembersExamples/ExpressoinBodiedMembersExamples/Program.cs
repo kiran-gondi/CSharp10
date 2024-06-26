874c0552cfdd352190beb9eac8506105ff2d2c5f
kiran.gondi <kiran.gondi@planview.com>
2024-06-26 12:39:34 +0530

    Expression Bodied Members

---
+++ Section-20/ExpressoinBodiedMembersExamples/ExpressoinBodiedMembersExamples/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ExpressoinBodiedMembersExamples
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //created student object
+      Student s = new Student() { StudentName = "Robbie" };
+
+      Console.WriteLine(s.StudentName);
+      Console.WriteLine(s.GetStudentNameLength());
+
+      Console.ReadKey();
+    }
+  }
+}
