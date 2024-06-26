f30082ae17b980926cfc08d9ef0d450514c5bded
kiran.gondi <kiran.gondi@planview.com>
2024-06-26 21:14:46 +0530

    System.Array.Resize

---
+++ Section-21/ArrayResizeExample/ArrayResizeExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArrayResizeExample
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //create array
+      int[] a = new int[3] { 10, 20, 30 };
+
+      //resize array to 5
+      //Array.Resize(ref a, 5);
+
+      //resize array to 2
+      Array.Resize(ref a, 2);
+
+      //print array
+      foreach (int i in a)
+      {
+        Console.WriteLine(i);
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
