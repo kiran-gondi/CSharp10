a2be51ab47bb9a69723a7d5d1f22062b3366d28d
kiran.gondi <kiran.gondi@planview.com>
2024-06-26 21:10:12 +0530

    System.Array.Clear

---
+++ Section-21/ArrayClearExample/ArrayClearExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArrayClearExample
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //create array
+      int[] a = new int[] { 10, 20, 30, 40, 50, 60 };
+
+      //clear elements of array
+      //Array.Clear(a, 0, a.Length
+      Array.Clear(a, 2, 3);
+
+      //display array
+      foreach (int item in a) {
+        Console.WriteLine(item);
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
