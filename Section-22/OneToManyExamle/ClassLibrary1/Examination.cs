4fa2caf26cb7fdfa1be86b7a64598ea627d62b4c
kiran.gondi <kiran.gondi@planview.com>
2024-06-29 17:06:30 +0530

    One-To-Many

---
+++ Section-22/OneToManyExamle/ClassLibrary1/Examination.cs
@@ -0,0 +1,19 @@
+﻿namespace CollegeOneToMany
+{
+  /// <summary>
+  /// Representes examination attempted by the student
+  /// </summary>
+  public class Examination
+  {
+      public string ExaminationName { get; set; }
+
+      public int Month { get; set; }
+
+      public int Year { get; set; }
+
+      public int MaxMarks { get; set; }
+
+      public int SecuredMarks { get; set; }
+
+  }
+}
---
+++ Section-22/OneToManyExamle/ClassLibrary1/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CollegeOneToMany
+{
+  /// <summary>
+  /// Represents a Student
+  /// </summary>
+  public class Student
+  {
+    public int RollNo { get; set; }
+    public string StudentName { get; set; }
+    public string Email { get; set; }
+
+    public List<Examination> Examinations { get; set; }
+  }
+}
