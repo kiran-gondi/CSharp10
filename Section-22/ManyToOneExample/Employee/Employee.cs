7e9c1aa934339e7aa359d895c7698841f49b87c5
kiran.gondi <kiran.gondi@planview.com>
2024-06-29 17:19:33 +0530

    ManyToOne

---
+++ Section-22/ManyToOneExample/Employee/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace ManyToOne
+{
+  /// <summary>
+  /// Employee class
+  /// </summary>
+  public class Employee
+  {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeEmail { get; set; }
+
+        public Department Dept { get; set; }
+
+      public override string ToString()
+      {
+      return "EmployeeId: " + EmployeeId + ", Employee Name: " + EmployeeName + ", " + "Employee Email"
+              + EmployeeEmail + "\nDept details: Department is " + Dept.DeptName + ", Dept Id: " + Dept.DeptId;
+      }
+  }
+}
