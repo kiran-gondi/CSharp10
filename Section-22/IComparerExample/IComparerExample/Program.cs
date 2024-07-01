d0e7f7acd01a590a080388d97f3f7a3eeb1cadfc
kiran.gondi <kiran.gondi@planview.com>
2024-07-01 22:43:12 +0530

    IComparer - Part 2

--- Section-22/IComparerExample/IComparerExample/Program.cs
+++ Section-22/IComparerExample/IComparerExample/Program.cs
@@ -12,15 +12,61 @@ namespace IComparerExample
       public string Job { get; set; }
     }
 
+    public enum SortBy
+    {
+      EmpId, EmpName, Job
+    }
+
     class CustomComparer : IComparer<Employee>
     {
+      //public int Compare(Employee x, Employee y)
+      //{
+      //  //Sort by EmployeeId
+      //  //return x.EmployeeId - y.EmployeeId;
+
+      //  //below one for string compare
+      //  return x.EmployeeName.CompareTo(y.EmployeeName);
+      //}
+
+      //Sort By Job, EmpName columns
+      //public int Compare(Employee x, Employee y)
+      //{
+      //  int result = 0;
+      //  if (x.Job != null) { 
+      //   result = x.Job.CompareTo(y.Job);
+      //  }
+      //  if (result == 0) {
+      //    if (x.EmployeeName != null)
+      //    {
+      //      result = x.EmployeeName.CompareTo(y.EmployeeName);
+      //    }
+      //  }
+      //  return result;  
+      //}
+
+      //Sortby selected column
       public int Compare(Employee x, Employee y)
       {
-        return x.EmployeeId - y.EmployeeId;
-        
-        //below one for string compare
-        //return x.EmployeeName.CompareTo(y.EmployeeName);
+        int result = 0;
+        switch (this.SortBy)
+        {
+          case SortBy.EmpId:
+            result = x.EmployeeId - y.EmployeeId;
+            break;
+          case SortBy.EmpName:
+            result = (x.EmployeeName != null) ? x.EmployeeName.CompareTo(y.EmployeeName) : 0; 
+            break;
+          case SortBy.Job:
+            result = (x.Job != null) ? x.Job.CompareTo(y.Job) : 0;
+            break;
+          default:
+            result = 0;
+            break;
+        }
+        return result;
       }
+
+      public SortBy SortBy { get; set; }
     }
 
     static void Main()
@@ -30,11 +76,14 @@ namespace IComparerExample
         new Employee(){EmployeeId = 104, EmployeeName = "Jon", Job = "Dev" },
         new Employee(){EmployeeId = 103, EmployeeName = "Joe", Job = "Design" },
         new Employee(){EmployeeId = 102, EmployeeName = "Morni", Job = "Consultant" },
-        new Employee(){EmployeeId = 101, EmployeeName = "Eveni", Job = "QA" }
+        new Employee(){EmployeeId = 101, EmployeeName = "Eveni", Job = "Consultant" },
+        new Employee(){EmployeeId = 105, EmployeeName = "Eveni", Job = null }
       };
 
       CustomComparer customComparer = new CustomComparer();
-      employees.Sort(customComparer);
+      customComparer.SortBy = SortBy.Job;
+      employees.Sort(customComparer);//EmployeeName
+
 
       Console.WriteLine();
       foreach (Employee employee in employees)
