18e4133ab0a568077fb16b78db4a789845ac8d90
kiran.gondi <kiran.gondi@planview.com>
2024-07-02 16:09:01 +0530

    Task.Run

---
+++ Section-32/TaskExample/TaskExample/Program.cs
@@ -0,0 +1,49 @@
+﻿class UpCounter
+{
+  public void CountUp(int count)
+  {
+    Console.WriteLine("\nCount-Up Starts");
+    for (int i = 1; i < count; i++)
+    {
+      Console.Write($"i = {i}, ");
+    }
+    Console.WriteLine("\nCount-Up Ends");
+  }
+}
+
+class DownCounter
+{
+  public void CountDown(int count)
+  {
+    Console.WriteLine("\nCount-Down Starts");
+    for (int j = count; j > 1; j--)
+    {
+      Console.Write($"j = {j}, ");
+    }
+    Console.WriteLine("\nCount-Down Ends");
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    //create objects for counter class
+    UpCounter upCounter = new UpCounter();
+    DownCounter downCounter = new DownCounter();
+
+    //create and start task
+    Task task1 = Task.Run(() =>
+    {
+      upCounter.CountUp(20);
+    });
+
+    Task task2 = Task.Run(() =>
+    {
+      downCounter.CountDown(20);
+    });
+
+
+    Console.ReadKey();
+  }
+}
