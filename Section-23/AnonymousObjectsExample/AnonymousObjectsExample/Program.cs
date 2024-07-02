039173586bfbeb58cce755c01b90d2793c6559cc
kiran.gondi <kiran.gondi@planview.com>
2024-07-02 10:46:36 +0530

    Anonymous Types

---
+++ Section-23/AnonymousObjectsExample/AnonymousObjectsExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace AnonymousObjectsExample
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //create object of Person clas
+      Person person = new Person();
+
+      //call methods
+      //Console.WriteLine(person.GetPersonName());
+      //Console.WriteLine(person.GetPersonAge());
+
+
+      var p1 = new { PersonName = person.GetPersonName(), PersonAge = person.GetPersonAge() };
+      Console.WriteLine(p1.PersonName + ", " + p1.PersonAge);
+
+      //p1.PersonName = "rahul";
+
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ Section-23/AnonymousObjectsExample/ClassLibrary1/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassLibrary1
+{
+    public class Person
+    {
+      public string GetPersonName()
+      {
+        return "Smith";
+      }
+
+      public int GetPersonAge()
+      {
+        return 22;
+      }
+    }
+}
