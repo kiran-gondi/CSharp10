ea22183fec3ba91da1fd88d38d4cd7475d358df7
kiran.gondi <kiran.gondi@planview.com>
2024-07-02 15:04:12 +0530

    Main Thread

---
+++ Section-31/ThreadingApp/ThreadingApp/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+  static void Main()
+  {
+    Thread mainThread = Thread.CurrentThread;
+    mainThread.Name = "Main thread demo";
+    Console.WriteLine(mainThread);
+    Console.WriteLine(mainThread.Name);
+    
+    Console.WriteLine("Main Method");
+    Method1();
+    Console.ReadKey();
+  }
+
+  static void Method1()
+  {
+    Console.WriteLine("Method 1");
+  }
+}
