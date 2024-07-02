7b26811bf5aecbcb87574031f81e31718df7fbb3
kiran.gondi <kiran.gondi@planview.com>
2024-07-02 10:48:37 +0530

    Nested Anonymous Types/Objects

--- Section-23/AnonymousObjectsExample/AnonymousObjectsExample/Program.cs
+++ Section-23/AnonymousObjectsExample/AnonymousObjectsExample/Program.cs
@@ -15,8 +15,10 @@ namespace AnonymousObjectsExample
       //Console.WriteLine(person.GetPersonAge());
 
 
-      var p1 = new { PersonName = person.GetPersonName(), PersonAge = person.GetPersonAge() };
+      var p1 = new { PersonName = person.GetPersonName(), PersonAge = person.GetPersonAge(), 
+        Address = new { Street = "abc", City = "hubli" } };
       Console.WriteLine(p1.PersonName + ", " + p1.PersonAge);
+      Console.WriteLine(p1.Address.City + ", " + p1.Address.Street);
 
       //p1.PersonName = "rahul";
 
