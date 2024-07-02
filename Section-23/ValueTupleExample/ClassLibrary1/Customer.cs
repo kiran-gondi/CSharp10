ce7a272c62283adb13150f01c9170d17a1d25692
kiran.gondi <kiran.gondi@planview.com>
2024-07-02 11:21:16 +0530

    Value Tuples

---
+++ Section-23/ValueTupleExample/ClassLibrary1/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Customer
+    {
+      public (int customerId, string customerName, string email) GetCustomerDetails()
+      {
+      return (101, "Rob", "rob@one.com");
+      }
+    }
+}
---
+++ Section-23/ValueTupleExample/ValueTupleExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTupleExample
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+
+      //create object
+      Customer customer = new Customer();
+
+      //get details
+      (int customerId, string customerName, string email) cust = customer.GetCustomerDetails();
+
+      Console.WriteLine(cust.customerId + ", " + cust.customerName + ", " + cust.email);
+
+      Console.ReadKey();
+    }
+  }
+}
