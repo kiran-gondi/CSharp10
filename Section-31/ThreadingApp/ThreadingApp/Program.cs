c2d9cf3f490fb94e3a358b42f12069916b120816
kiran.gondi <kiran.gondi@planview.com>
2024-07-02 15:25:34 +0530

    Thread Class

--- Section-31/ThreadingApp/ThreadingApp/Program.cs
+++ Section-31/ThreadingApp/ThreadingApp/Program.cs
@@ -1,14 +1,23 @@
-﻿class Program
+﻿using System.Threading;
+class Program
 {
   static void Main()
   {
     Thread mainThread = Thread.CurrentThread;
     mainThread.Name = "Main thread demo";
-    Console.WriteLine(mainThread);
-    Console.WriteLine(mainThread.Name);
+    //Console.WriteLine(mainThread);
+    Console.WriteLine(mainThread.Name);//Main thread demo"
+
+    ThreadPriority tp = mainThread.Priority;
+    Console.WriteLine(tp.ToString());//Normal
+    Console.WriteLine(mainThread.IsAlive);//True
+    Console.WriteLine(mainThread.IsBackground);//False
     
-    Console.WriteLine("Main Method");
-    Method1();
+    ThreadState ts = mainThread.ThreadState;
+    Console.WriteLine(ts.ToString());//Running
+
+    //Console.WriteLine("Main Method");
+    //Method1();
     Console.ReadKey();
   }
 
