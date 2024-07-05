b17acf62454ab6eb41e1bba03d814b3750e1eb0c
kiran.gondi <kiran.gondi@planview.com>
2024-07-05 23:38:52 +0530

    Nullable Reference Types

---
+++ Section-30/NullableReferenceTypes/NullableReferenceTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿class Employee
+{
+  public int x{ get; set; }
+}
+
+class EmployeeBusinessLogic
+{
+  public Employee? GetEmployee()
+  {
+    //return new Employee() { x = 10 };
+    return null;
+  }
+}
+//Emloyee = non-nullable (null values are not expected)
+//Emloyee? = nullable (accept null values)
+class Program
+{
+  static void Main()
+  {
+    EmployeeBusinessLogic employeeBusinessLogic = new EmployeeBusinessLogic();
+    Employee? employee = employeeBusinessLogic.GetEmployee();
+    if(employee !=null)
+      Console.WriteLine(employee.x);
+    Console.ReadKey();
+  }
+}
