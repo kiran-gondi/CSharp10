5d8a09ee7857ccc71c2c002ff6bb3d63325817d5
kiran.gondi <kiran.gondi@planview.com>
2024-07-05 23:53:04 +0530

    Null Forgiving Operator & Null Propagation Operator

--- Section-30/NullableReferenceTypes/NullableReferenceTypes/Program.cs
+++ Section-30/NullableReferenceTypes/NullableReferenceTypes/Program.cs
@@ -1,14 +1,31 @@
 ﻿class Employee
 {
   public int x{ get; set; }
+
+  public string FirstName { get; set; }
+  public string LastName { get; set; }
+
+  public AddressDetails? PersonAddressDetails { get; set; }
+
+    public Employee()
+    {
+      FirstName = "test1";
+      LastName = "test2";
+    }
+}
+
+class AddressDetails()
+{
+    public string? City { get; set; }
+    public int? ZipCode { get; set; }
 }
 
 class EmployeeBusinessLogic
 {
   public Employee? GetEmployee()
   {
-    //return new Employee() { x = 10 };
-    return null;
+    return new Employee() { x = 10 };
+    //return null;
   }
 }
 //Emloyee = non-nullable (null values are not expected)
@@ -19,8 +36,21 @@ class Program
   {
     EmployeeBusinessLogic employeeBusinessLogic = new EmployeeBusinessLogic();
     Employee? employee = employeeBusinessLogic.GetEmployee();
-    if(employee !=null)
-      Console.WriteLine(employee.x);
+    bool isValid = employee != null;
+
+    if (isValid) { 
+      Console.WriteLine(employee!.x); //Null Forgiving Operator
+      Console.WriteLine(employee!.FirstName.ToUpper());
+      Console.WriteLine(employee!.LastName.ToUpper());
+      //if (employee!.PersonAddressDetails != null)
+      //{
+      //  Console.WriteLine(employee!.PersonAddressDetails.City);
+      //  Console.WriteLine(employee!.PersonAddressDetails!.ZipCode);
+      //}
+      Console.WriteLine(employee!.PersonAddressDetails?.City);
+      Console.WriteLine(employee!.PersonAddressDetails?.ZipCode);
+    }
+
     Console.ReadKey();
   }
 }
