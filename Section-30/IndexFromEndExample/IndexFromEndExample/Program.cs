56085173409a48a932dd55e954ab0ea963c1d93f
kiran.gondi <kiran.gondi@planview.com>
2024-07-08 18:48:39 +0530

    Range Struct

--- Section-30/IndexFromEndExample/IndexFromEndExample/Program.cs
+++ Section-30/IndexFromEndExample/IndexFromEndExample/Program.cs
@@ -8,4 +8,19 @@ Console.WriteLine(myFriends[^5]);//Scott
 Index i = ^4;
 Console.WriteLine(myFriends[i]);//Allen
 
+//Range-LINQ
+//IEnumerable<string> shortListedFriends = myFriends.Skip(1).Take(3);
+
+//Range Struct
+Range r = 1..4;
+IEnumerable<string> shortListedFriends = myFriends[r];//myFriends[1..4];
+Console.WriteLine("Short Listed Friends");
+foreach (string s in shortListedFriends)
+{
+  Console.Write(s + ", ");
+}
+
+
+
+
 Console.ReadKey();
