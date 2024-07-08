3e4bd4334631659d3be76926d219c1472e86fd59
kiran.gondi <kiran.gondi@planview.com>
2024-07-08 16:22:11 +0530

    CLI-Part 1

---
+++ Section-30/CLIExample1/CLIExample1/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+
+    Console.WriteLine($"{args.Length} values found." );
+    int sum = 0;
+    foreach (string i in args) {
+      if(int.TryParse(i, out int result))
+      {
+        sum += result;
+      }
+    }
+    Console.WriteLine($"Total of all numbers: {sum}");
+    Console.ReadKey();
+    //NOTE: Run this solution via command prompt with Command Line Arguments(CLI).
+  }
+}
