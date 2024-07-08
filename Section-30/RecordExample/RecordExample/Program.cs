b50e41986d549bf5cbeb5047191f1f675b482128
kiran.gondi <kiran.gondi@planview.com>
2024-07-08 12:46:58 +0530

    Records - Equality

--- Section-30/RecordExample/RecordExample/Program.cs
+++ Section-30/RecordExample/RecordExample/Program.cs
@@ -1,5 +1,4 @@
-﻿using static records_example.Person;
-
+﻿
 namespace records_example
 {
   //C# 9 Records
@@ -9,23 +8,31 @@ namespace records_example
   //}
 
   //Nested Records
-  public record Address(string city)
-  {
-    public string? Country { get; set; }
-  }
+  public record Address(string City, string Country);
+  //{
+  //  public string? Country { get; set; }
+  //}
 
   class Program
   {
     static void Main()
     {
-      Person p1 = new Person("Bill1", 22, new Address("London"));
-      Person p2 = new("Bill2", 32, new Address("Delhi"));
-      p1.PersonAddress.Country = "UK";
+      Person p1 = new Person("Bill1", 22, new Address("London", "UK"));
+      //Person p2 = new("Bill2", 32, new Address("Indi"Delhi"));
+      //p1.PersonAddress.Country = "UK";
+      //Person p2 = new Person("Bill1", 22, new Address("London", "UK"));
+      Person p2 = new Person("Bill1", 22, p1.PersonAddress);
 
-
-      Console.WriteLine($"{p1.Name}, {p1.Age}, {p1.PersonAddress.city}, {p1.PersonAddress.Country}");
+      Console.WriteLine($"{p1.Name}, {p1.Age}, {p1.PersonAddress.City}, {p1.PersonAddress.Country}");
+      Console.WriteLine($"{p2.Name}, {p2.Age}, {p2.PersonAddress.City}, {p2.PersonAddress.Country}");
       //Console.WriteLine($"{p2.Name}, {p2.Age}, {p2.PersonAddress.city}");
       //p1.Name = "jack"; //Error: Init-only property can only be assigned in a constructor or object initializer.
+
+      Console.WriteLine(p1.Equals(p2));//True
+      Console.WriteLine(p1 == p2);//True
+      Console.WriteLine(p1 != p2);//False
+      //Console.WriteLine(p1 != p2.PersonAddress);//Error
+
       Console.ReadKey();
     }
   }
