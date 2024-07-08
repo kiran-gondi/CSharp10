7abea2a30e5cdfcd6877d56ab653e88c6a1df104
kiran.gondi <kiran.gondi@planview.com>
2024-07-08 15:29:26 +0530

    Records - ToString()

--- Section-30/RecordExample/RecordExample/Program.cs
+++ Section-30/RecordExample/RecordExample/Program.cs
@@ -7,17 +7,28 @@ namespace records_example
   //  //public int x { get; set; }
   //}
 
-//Records - 'with' Expression
-//public record Person(string Name, Address PersonAddress)
-//{
-//  public int Age { get; set; }
-//}
+  //Records - 'with' Expression
+  //public record Person(string Name, Address PersonAddress)
+  //{
+  //  public int Age { get; set; }
+  //}
 
-//Nested Records
-public record Address(string City, string Country);
+  //Nested Records
+  //public record Address(string City, string Country);
   //{
   //  public string? Country { get; set; }
   //}
+
+  //Records - ToString()
+  #region Records - ToString()
+  public record Address(string City, string Country)
+  {
+    public override string ToString()
+    {
+      return $"City is {City} in {Country}";
+    }
+  } 
+  #endregion
 
   class Program
   {
@@ -33,12 +44,14 @@ public record Address(string City, string Country);
       //Person p2 = p1 with { Age = 90, Name="rob" }; //Shallow Copy
       //p1.Age = 55;
 
+      #region Records Deconstruct()
       //var (name, age, add)=p1;
       //var (name, age, (city, country))=p1;
-      var (name, _, (city, country))=p1;//Discard if we need to discard a value.
+      //var (name, _, (city, country))=p1;//Discard if we need to discard a value.
 
       //Console.WriteLine($"{name}, {age}, {city}, {country}");
-      Console.WriteLine($"{name}, {city}, {country}");
+      //Console.WriteLine($"{name}, {city}, {country}");
+      # endregion
 
       //Console.WriteLine($"{p1.Name}, {p1.Age}, {p1.PersonAddress.City}, {p1.PersonAddress.Country}");
       //Console.WriteLine($"{p2.Name}, {p2.Age}, {p2.PersonAddress.City}, {p2.PersonAddress.Country}");
@@ -49,6 +62,9 @@ public record Address(string City, string Country);
       //Console.WriteLine(p1 == p2);//True
       //Console.WriteLine(p1 != p2);//False
       //Console.WriteLine(p1 != p2.PersonAddress);//Error
+
+      //Records - ToString()
+      Console.WriteLine(p1.ToString());
 
       Console.ReadKey();
     }
