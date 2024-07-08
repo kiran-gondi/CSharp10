5ce2579cf0ba64dfc30505747e0012f9d2bdb556
kiran.gondi <kiran.gondi@planview.com>
2024-07-08 17:28:49 +0530

    Interface Default Methods

---
+++ Section-30/InterfaceDefaultMethodsExample/InterfaceDefaultMethodsExample/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace interface_enhancements
+{
+  public interface IEmployee
+  {
+      public string Name { get; set; }
+
+    //default interface methods OR virtual extension method
+    public string GetNameInUpperCase()
+    {
+      return Name.ToUpper();
+    }
+  }
+
+  public class Manager : IEmployee
+  {
+    private string _name;
+    public string Name { get => _name; set => _name = value; }
+  }
+
+  public class Clerk : IEmployee
+  {
+    private string _name;
+    public string Name { get => _name; set => _name = value; }
+  }
+
+  class Program
+  {
+    static void Main()
+    {
+      Manager m = new Manager() { Name = "Bob" };
+      //IEmployee m = new Manager() { Name = "Bob" };
+      IEmployee iemp = (IEmployee)m;
+      Console.WriteLine(iemp.GetNameInUpperCase());
+      Console.ReadKey();
+    }
+  }
+
+}
+
