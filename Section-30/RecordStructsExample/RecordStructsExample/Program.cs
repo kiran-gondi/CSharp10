b1fe05d84b4239a086960facfb419d4c413bd54b
kiran.gondi <kiran.gondi@planview.com>
2024-07-08 16:11:42 +0530

    Record Structs

---
+++ Section-30/RecordStructsExample/RecordStructsExample/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace record_structs_example
+{
+  //public readonly record struct Person(string? Name, int? Age);
+  public record struct Person(string? Name, int? Age);
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Person person = new Person("Jaxon", 30);
+      //ToString(), Deconstruct(), Equals()
+      Console.WriteLine(person.ToString());
+
+      Person p2 = person;
+      person.Age = 40;//will not effect the other struct variable(p2)
+
+      Console.WriteLine(p2.ToString());
+      Console.ReadKey();
+    }
+  }
+}
