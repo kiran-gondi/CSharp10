f783f8665bbc9eab7dcc72cdd545dcaa1f794029
kiran.gondi <kiran.gondi@planview.com>
2024-07-08 18:37:02 +0530

    Index-From-End Operator

---
+++ Section-30/IndexFromEndExample/IndexFromEndExample/Program.cs
@@ -0,0 +1,11 @@
+﻿//create an List of strings
+List<string> myFriends = new List<string>() { "Scott", "Allen", "Jones", "James", "Sara"};
+Console.WriteLine(myFriends[myFriends.Count - 1]);
+
+Console.WriteLine(myFriends[^1]);//Sara
+Console.WriteLine(myFriends[^5]);//Scott
+
+Index i = ^4;
+Console.WriteLine(myFriends[i]);//Allen
+
+Console.ReadKey();
