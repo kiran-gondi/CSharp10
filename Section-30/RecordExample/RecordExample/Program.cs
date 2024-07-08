8b1c148d9854b521b4c6c8663eda0c88a09f2ccf
kiran.gondi <kiran.gondi@planview.com>
2024-07-08 12:00:23 +0530

    Records - Basics

---
+++ Section-30/RecordExample/RecordExample/Program.cs
@@ -0,0 +1,15 @@
+﻿//C# 9 Records
+public record Person(string Name, int Age);
+
+class Program
+{
+  static void Main()
+  {
+    Person p1 = new Person("Bill1", 22);
+    Person p2 = new("Bill2", 32);
+    Console.WriteLine($"{p1.Name}, {p1.Age}");
+    Console.WriteLine($"{p2.Name}, {p2.Age}");
+    //p1.Name = "jack"; //Error: Init-only property can only be assigned in a constructor or object initializer.
+    Console.ReadKey();
+  }
+}
