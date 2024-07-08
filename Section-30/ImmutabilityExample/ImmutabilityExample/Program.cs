a39bbd9e43e5d866c0e6a0a4724f9fd3a0dbabda
kiran.gondi <kiran.gondi@planview.com>
2024-07-08 11:28:18 +0530

    Need of Immutability

---
+++ Section-30/ImmutabilityExample/ImmutabilityExample/Program.cs
@@ -0,0 +1,37 @@
+﻿class BankAccount
+{
+    public int AccountNumber { get; set; }
+    public double CurrentBalance { get; set; }
+}
+
+class DataStorage
+{
+  //developer1
+  public static List<BankAccount> GetBankAccounts()
+  {
+    return new List<BankAccount>() { 
+      new BankAccount(){AccountNumber=1, CurrentBalance=1000},
+      new BankAccount(){AccountNumber=2, CurrentBalance=2000}
+    };
+  }
+
+  //developer2
+  public static double GetCurrentBalance(BankAccount bankAccount)
+  {
+    bankAccount.AccountNumber = 100; //Unexpectedly it changes the value of Account Number
+    return bankAccount.CurrentBalance;
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    List<BankAccount> bankAccounts = DataStorage.GetBankAccounts();
+    BankAccount firstBankAccount = bankAccounts[0];
+    double firstBankAccountBalance = DataStorage.GetCurrentBalance(firstBankAccount);
+    Console.WriteLine(firstBankAccountBalance);
+
+    Console.ReadKey();
+  }
+}
