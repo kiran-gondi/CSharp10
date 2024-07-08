2eee544cbf03f4efd90ebc4813008942fcc27d6e
kiran.gondi <kiran.gondi@planview.com>
2024-07-08 15:44:19 +0530

    Records - Constructor

--- Section-30/RecordExample/RecordExample/Program.cs
+++ Section-30/RecordExample/RecordExample/Program.cs
@@ -2,7 +2,7 @@
 namespace records_example
 {
   //C# 9 Records
-  public record Person(string Name, int Age, Address PersonAddress);
+  //public record Person(string Name, int Age, Address PersonAddress);
   //{
   //  //public int x { get; set; }
   //}
@@ -27,17 +27,44 @@ namespace records_example
     {
       return $"City is {City} in {Country}";
     }
-  } 
+  }
+  #endregion
+
+  #region Records - Construtor
+  //Records - Construtor
+  
+  public record Person(string? Name, DateTime? DateOfBirth, int? Age)
+  {
+    //User ==> user-defined constructor --> compiler-generated constructor
+    public Person(string? name, DateTime? dateOfBirth) : this(name, dateOfBirth, null)
+    {
+      if (dateOfBirth != null) {
+        Age = Convert.ToInt32 ((DateTime.Now.Subtract(dateOfBirth.Value).TotalDays / 365.25));
+      }
+    }
+
+    //user-defined constructor (paramter-less)
+    public Person() : this(null, null, null)
+    {
+      
+    }
+
+    //user-defined method
+    public string GetName()
+    {
+      return $"Mr./Ms.{Name}";
+    }
+  }
   #endregion
 
   class Program
   {
     static void Main()
     {
-      Person p1 = new Person("Bill1", 20, new Address("London", "UK"));
+      //Person p1 = new Person("Bill1", 20, new Address("London", "UK"));
       //Person p2 = new("Bill2", 32, new Address("Indi"Delhi"));
       //p1.PersonAddress.Country = "UK";
-      Person p2 = new Person("Bill1", 22, new Address("London", "UK"));
+      //Person p2 = new Person("Bill1", 22, new Address("London", "UK"));
       //Person p2 = new Person("Bill1", 22, p1.PersonAddress);
       //p1.Age = 20;
       ////Person p2 = p1; //Reference Copy
@@ -64,7 +91,12 @@ namespace records_example
       //Console.WriteLine(p1 != p2.PersonAddress);//Error
 
       //Records - ToString()
-      Console.WriteLine(p1.ToString());
+      //Console.WriteLine(p1.ToString());
+
+      //Records - Constructor
+      Person p1 = new Person("Scott", DateTime.Parse("2001-06-04"));
+      Console.WriteLine(p1);
+      Console.WriteLine(p1.GetName());
 
       Console.ReadKey();
     }
