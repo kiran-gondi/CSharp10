232b7689f7095fd659fe8b7284c6ec7c01da9167
kiran.gondi <kiran.gondi@planview.com>
2025-01-04 17:10:40 +0530

    Format Exception

---
+++ Section-28/FormatException/FormatException/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace FormatExceptionDemo
+{
+    class BankAccount
+    {
+        public string AccountHolderName { get;set; }
+        public int AccountNumber { get; set; }
+        public double CurrentBalance { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //create object of BankAccount
+                BankAccount bankAccount = new BankAccount();
+
+                //input from keyboard
+                Console.Write("Enter account holder name:");
+                bankAccount.AccountHolderName = Console.ReadLine();
+
+                Console.Write("Enter account number:");
+                bankAccount.AccountNumber = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter current balance:");
+                bankAccount.CurrentBalance = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\n New Bank account details:");
+
+                Console.Write("Account holder name:" + bankAccount.AccountHolderName);
+                Console.Write("Account number:" + bankAccount.AccountNumber);
+                Console.Write("Current Balance:" + bankAccount.CurrentBalance);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
